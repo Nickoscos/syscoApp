59784a4f95182d9ae35457e34a5a7e81cffd00a2
Nickoscos <nicolas.pelleschi@gmail.com>
2023-04-23 21:48:39 +0200

    settings pour publication

--- DEFPTS/settings.py
+++ DEFPTS/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m=z+!+*%=5rhqe302)2ve6+0@jh%+**eeo_@z-$ss5vlzczy@p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['Nickoscos.pythonanywhere.com']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nickoscos.pythonanywhere.com']
 
 # Application definition
 
