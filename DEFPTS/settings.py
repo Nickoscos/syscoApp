306236017d0014d95aea3bcf38f1d88c8bb74c70
Nickoscos <nicolas.pelleschi@gmail.com>
2023-04-27 14:28:37 +0200

    change settings

--- DEFPTS/settings.py
+++ DEFPTS/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    BASE_DIR / "polls/static/",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "polls/static/",
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
