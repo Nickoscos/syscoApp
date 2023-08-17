fd9c8719aa6a7f763f2a230e78f10ce4a1dd7824
Nickoscos <nicolas.pelleschi@gmail.com>
2023-08-17 17:56:08 +0200

    début vue pack

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .viewsPage import listeView
+from .viewsPage import listeView, packsView
 from .ListePTS import export
 
 app_name = 'polls'
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', listeView.genListeView, name='chaufferie'),
     path('liste/', listeView.listePts, name='listePts'),
     path('downloadfile/<str:filename>', export.download_file, name='downloadfile'),
+    path('packs/', packsView.choicePack, name='packsView'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
