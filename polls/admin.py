65b2393ae0f0148b3ddb6b0e4910ffd7541363bf
Nickoscos <nicolas.pelleschi@gmail.com>
2023-11-03 10:04:35 +0100

    ajout import export catalogue wit

--- polls/admin.py
+++ polls/admin.py
@@ -5,9 +5,11 @@ from .models.Typology.modelsEquip import Point
 from .models.Pack.modelsPacks import PackTG, PackOPT, PackIOTUnit
 from .models.Pack.modelsAutom import carteAutom
 
+from import_export.admin import ImportExportActionModelAdmin
+
 admin.site.register(Chaufferie)
 admin.site.register(Point)
 admin.site.register(PackTG)
 admin.site.register(PackOPT)
 admin.site.register(PackIOTUnit)
-admin.site.register(carteAutom)
+admin.site.register(carteAutom, ImportExportActionModelAdmin)
